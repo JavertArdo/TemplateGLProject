1146c045346f71add80dcf9d69ade235165471aa
Javert Ardo <frank@10.0.0.8>
2018-06-09 12:40:05 +0200

    Linked GLEW libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
+#include <GL/glew.h>
+#include <GL/glut.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main(void)
 {
 	GLFWwindow* window;
 
+	// Initializing GLFW
 	if(!glfwInit())
 		return -1;
 
@@ -15,9 +20,23 @@ int main(void)
 
 	glfwMakeContextCurrent(window);
 
+	// Initializing GLEW
+	if (GLEW_OK != glewInit())
+		return -1;
+
+	// OpenGL Version
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
 	while(!glfwWindowShouldClose(window)) {
 
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Draw triangle
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f( 0.0f,  0.5f);
+		glVertex2f( 0.5f, -0.5f);
+		glEnd();
 
 		glfwSwapBuffers(window);
 
